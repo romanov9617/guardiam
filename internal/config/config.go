246ee6bf5fa4a77c42f1e581104d1ab75b3e30ef
Danil Romanov <danil.romanov@red-soft.ru>
2025-07-15 21:41:28 +0300

    fix: update pre commit

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Config struct {
+	API      APIConfig      `json:"api"`
+	Database DatabaseConfig `json:"database"`
+	Logger   LoggerConfig   `json:"logger"`
+}
+
+type APIConfig struct {
+	Host string `json:"host"`
+	Port int    `json:"port"`
+
+	ShutdownIn int `json:"shutdown_in"`
+}
+
+type DatabaseConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+type LoggerConfig struct {
+	Environment string `json:"environment"`
+}
+
+func LoadConfig(source io.Reader) (*Config, error) {
+	var cfg = new(Config)
+
+	decoder := json.NewDecoder(source)
+	err := decoder.Decode(cfg)
+
+	return cfg, err
+}
